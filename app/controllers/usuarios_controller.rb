ee3147f5145791f1374cdfd7403caa9684f26426
RaulCE22 <rce1991@gmail.com>
2015-07-17 20:11:01 +0200

    Ya se puede editar el usuario. Añadirle imagen y modificar los atributos

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -5,9 +5,22 @@ class UsuariosController < ApplicationController
     @me = current_donante
 
   end
-  def edit
 
-    @me = Usuarios.current
+  def edit
+    @me = current_donante
+
   end
 
+  def update
+    @me = current_donante
+    if @me.update_attributes(user_params)
+      redirect_to(usuarios_index_path)
+    else
+      render 'edit'
+    end
+  end
+
+  def user_params
+    params.require(:donante).permit(:name, :email, :description, :contacto, :avatar)
+  end
 end
