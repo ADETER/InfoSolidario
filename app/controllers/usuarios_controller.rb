740d0a0afd452b87d508d90d663d01892156fb89
ADETER <informatico@adeter.org>
2015-07-21 18:56:14 +0200

    puta mierda

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -2,7 +2,7 @@ class UsuariosController < ApplicationController
   def index
 
   	@donacion = DonacionTiempo.all + DonacionRecurso.all
-    @me = current_donante
+    @me = current_donante || current_ong
     @usuariosconnect = donante_signed_in? || ong_signed_in? || empresa_signed_in?
 
   end
